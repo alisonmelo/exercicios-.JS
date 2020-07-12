77a9c991eea47758e0c8efb7ed7997dd0df6eec5
alisonmelo <61011223+alisonmelo@users.noreply.github.com>
2020-07-11 23:50:09 -0300

    Add files via upload

---
+++ aula 14/ambiente.js
@@ -0,0 +1,26 @@
+for (var calculo = 1; calculo <= 10; calculo++) {
+    console.log(`passo ${contador}`)
+}
+
+
+
+
+/* let contador = 1
+do {
+    console.log(`passo ${contador}`)
+    contador++ // contador = contador + 1
+} while (contador <= 6)
+ */
+/* 
+let contador = 1
+while (contador <= 6) {
+    console.log(`passo ${contador}`)
+    contador++ // contador = contador + 1
+} */
+
+
+/* console.log(`all bad ;-;`)
+console.log(`all bad ;-;`)
+console.log(`all bad ;-;`)
+console.log(`all bad ;-;`)
+console.log(`all bad ;-;`) */
